f792b5f1fba5d9c90732b6eb2f13319948e5e661
Luka TALPAIN <l_talpain@stu-bellecour.fr>
2026-03-13 14:52:43 +0100

    Condition et effets

---
+++ Assets/Scripts/SO/pieces/Conditions/SoCondition.cs
@@ -0,0 +1,12 @@
+using System.IO.Enumeration;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class SoCondition : ScriptableObject
+{
+    public bool Condition () //  ajouter context 
+    {
+        return true;
+    }
+}
---
+++ Assets/Scripts/SO/pieces/Context.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using Unity.VisualScripting.FullSerializer;
+
+public class Context 
+{
+    public int Tour;
+
+    public int NbrCaseDeLaPiece;
+
+    public int NbrPiecesAutour;
+    public int NbrCaseLibre;
+    public int NbrCaseOccupé;
+
+    public int NbrDeRepetition;
+
+    // script de stats joueur 
+    // script de stats ennemi 
+
+
+
+
+}
