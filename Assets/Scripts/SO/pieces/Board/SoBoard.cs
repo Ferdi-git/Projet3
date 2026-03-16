8127b18803fcc6aa9cdce3760fcb836af551d170
Luka TALPAIN <l_talpain@stu-bellecour.fr>
2026-03-16 12:56:19 +0100

    RenameBoard

--- Assets/Scripts/SO/pieces/Board/SoBoard.cs
+++ Assets/Scripts/SO/pieces/Board/SoBoard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class Board : ScriptableObject
+public class SoBoard : ScriptableObject
 {
     public List<BoardPiece> boardPieces;
 }
