00f15f9dd6c8bdfa29584645c6813ecffd0a660a
Luka TALPAIN <l_talpain@stu-bellecour.fr>
2026-03-16 14:00:15 +0100

    r

--- Assets/Scripts/SO/pieces/Board/BoardPiece.cs
+++ Assets/Scripts/SO/pieces/Board/BoardPiece.cs
@@ -5,7 +5,6 @@ public class BoardPiece
 {
     public SoPieces soPieces;
     public PiecePersonality piecePersonality;
-    [HideInInspector] public BoardPiece[] voisins;
     public Context context;
     
     
