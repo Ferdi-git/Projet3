e0e3896e0d1bddd2dbf70f52a999a79f369febbb
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2026-03-22 18:39:24 +0100

    Fixed Damage nbr visual

--- Assets/Scripts/VisualSpawner.cs
+++ Assets/Scripts/VisualSpawner.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 public class VisualSpawner : MonoBehaviour
 {
     [SerializeField] GameObject prefabVisuel;
+    [SerializeField] Transform spawnPoint;
+
     [Button]
     public void CreateVisual()
     {
         float randFloat = Random.Range(0,4.5f) ;
         float randFloat2 = Random.Range(0,2f) ;
-        Vector2 pos = new Vector2(transform.position.x + randFloat, transform.position.y + randFloat2);
+        Vector2 pos = new Vector2(spawnPoint.position.x + randFloat, spawnPoint.position.y + randFloat2);
 
-        GameObject newSingle = Instantiate(prefabVisuel , pos, transform.rotation , transform);
-        
-        //newSingle.GetComponent<SingleUIVisuel>().Initialise();    
+        GameObject newSingle = Instantiate(prefabVisuel, spawnPoint.position, transform.rotation, transform);
+
+
+        newSingle.transform.localPosition += new Vector3(randFloat, randFloat2, 0f);
+
+
+        DataUIVisuel dataUIVisuel = new DataUIVisuel();
+        dataUIVisuel.textColor = Random.ColorHSV();
+        dataUIVisuel.nbr = Random.Range(10, 1000);
+
+
+        newSingle.GetComponent<SingleUIVisuel>().Initialise(dataUIVisuel);    
     }
+
 
 
 }
