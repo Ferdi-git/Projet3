6b0799cff9759e44d3d36cbfb35bf5e2510fa956
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2026-03-28 22:18:26 +0100

    Damn thats satisfying

--- Assets/Scripts/Piece/PieceInfoPanel.cs
+++ Assets/Scripts/Piece/PieceInfoPanel.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ public class PieceInfoPanel : MonoBehaviour
     Canvas canvas;
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] GameObject panelInfo;
+    [SerializeField] float timeBeforeApearing = 0.15f;
+    float baseScaleY;
+
 
     private void Awake()
     {
@@ -16,23 +20,32 @@ public class PieceInfoPanel : MonoBehaviour
     }
     private void Start()
     {
+        baseScaleY = panelInfo.transform.localScale.y;
         HidePanel();
         Initialyse();
     }
 
     public void Initialyse()
     {
+        text.gameObject.SetActive(false);
         text.text = pieceInfo.soPiece.description;
     }
 
     public void ShowPanel()
     {
+        panelInfo.transform.DOKill();
+        text.gameObject.SetActive (false);
+        panelInfo.transform.localScale = new Vector3(panelInfo.transform.localScale.x , 0, panelInfo.transform.localScale.z) ;
         panelInfo.SetActive(true);
+        panelInfo.transform.DOScaleY(baseScaleY,0.2f).SetDelay(timeBeforeApearing).OnComplete(() => text.gameObject.SetActive(true));
+
     }
 
     public void HidePanel()
     {
-        panelInfo.SetActive(false);
+        panelInfo.transform.DOKill();
+        text.gameObject.SetActive(false);
+        panelInfo.transform.DOScaleY(0, 0.2f).OnComplete(() => panelInfo.SetActive(false));
 
     }
 
