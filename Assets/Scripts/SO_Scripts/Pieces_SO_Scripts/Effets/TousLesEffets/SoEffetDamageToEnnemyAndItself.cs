63c6fa4f75c61e82b31f79d3b14362cf82098dc4
ravioli-sec <malo.tasse@gmail.com>
2026-04-17 14:43:07 +0200

    Damage Itelf Piece

---
+++ Assets/Scripts/SO_Scripts/Pieces_SO_Scripts/Effets/TousLesEffets/SoEffetDamageToEnnemyAndItself.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DamageToEnnemyAndItself", menuName = "Effet/DamageToEnnemyAndItself")]
+
+public class SoEffetDamageToEnnemyAndItself : SoEffet
+{
+    public override IEnumerator Effet(Context context, OutputPort port, List<int> amount, int tour)
+    {
+        port.piecePlayed.PiecePlayedUp();
+        port.DoDamageToEnnemi(amount[0]);
+        port.thisBoardPiece.healthPoint -= amount[1];
+        yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.atk, null);
+    }
+    public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
+    {
+        port.piecePlayed.RepeatedPieceUp();
+        port.DoDamageToEnnemi(amount[0]);
+        port.thisBoardPiece.healthPoint -= amount[1];
+        yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPieceRepeated(), PieceAnimations.TypeAnim.atk, declencheur);
+    }
+}
