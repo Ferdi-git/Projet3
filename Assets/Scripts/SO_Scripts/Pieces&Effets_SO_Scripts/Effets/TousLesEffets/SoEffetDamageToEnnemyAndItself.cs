0ea32338acbe6698e9bdb9a27b30257db297453b
ravioli-sec <malo.tasse@gmail.com>
2026-04-20 10:41:37 +0200

    Encore des pièces

--- Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageToEnnemyAndItself.cs
+++ Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageToEnnemyAndItself.cs
@@ -11,14 +11,14 @@ public class SoEffetDamageToEnnemyAndItself : SoEffet
     {
         port.piecePlayed.PiecePlayedUp();
         port.DoDamageToEnnemi(amount[0]);
-        port.PlayerTakeDamage(amount[1]);
+        //port.PlayerTakeDamage(amount[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.atk, null);
     }
     public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
     {
         port.piecePlayed.RepeatedPieceUp();
         port.DoDamageToEnnemi(amount[0]);
-        port.PlayerTakeDamage(amount[1]);
+        //port.PlayerTakeDamage(amount[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPieceRepeated(), PieceAnimations.TypeAnim.atk, declencheur);
     }
 }
