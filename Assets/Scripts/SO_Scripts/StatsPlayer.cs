8d177100ee8bd7dd5c6618ac1cf28a69121812df
Ferdinand MOURETTE <f_mourette@stu-bellecour.fr>
2026-04-20 09:39:47 +0200

    context autour piece

--- Assets/Scripts/SO_Scripts/StatsPlayer.cs
+++ Assets/Scripts/SO_Scripts/StatsPlayer.cs
@@ -7,22 +7,46 @@ public class StatsPlayer : ScriptableObject
 {
     public int pv;
     public int shield;
+    public int mana;
+
 
     public int pvMax;
+    public int manaMax;
 
     public SOEventPlayer player;
 
     
     public void InvokeGainPV(int amount ) 
     {
-        pv += amount;
-        player.InvokePlayerGainPV( amount );
+        if(pv + amount > pvMax) pv = pvMax;
+        else
+        {
+            pv += amount;
+        }
+        player.InvokePlayerGainPV(amount);
     }
     public void InvokeLoosePV(int amount) 
     { 
         pv -= amount;
         player.InvokePlayerLoosePV( amount );
     }
+
+
+    public void InvokeGainMana(int amount)
+    {
+        if (mana + amount > manaMax) mana = manaMax;
+        else
+        {
+            mana += amount;
+        }
+        player.InvokePlayerGainMana(amount);
+    }
+    public void InvokeLooseMana(int amount)
+    {
+        mana -= amount;
+        player.InvokePlayerLooseMana(amount);
+    }
+
 
     public void InvokeGainShield(int amount) { shield += amount; player.InvokePlayerGainShield(amount); }
     public void InvokeTakeDamage(int amount) 
