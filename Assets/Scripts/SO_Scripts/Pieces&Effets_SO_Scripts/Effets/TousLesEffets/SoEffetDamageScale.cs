97cf6c4e19163ffe61b5c5831ec3c2889dc53084
ravioli-sec <malo.tasse@gmail.com>
2026-05-02 20:20:46 +0200

    Pieces Version Finale pour le test

--- Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScale.cs
+++ Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScale.cs
@@ -3,23 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SoEffetDamageProportionnalEmptyEffet", menuName = "Effet/DamageProportionnalEmpty")]
-
-public class SoEffetDamageProportionnalEmpty : SoEffet
+[CreateAssetMenu(fileName = "DamageScaleEffet", menuName = "Effet/DamageScale")]
+public class SoEffetDamageScale : SoEffet
 {
     public override IEnumerator Effet(Context context, OutputPort port, List<int> amount, int tour)
     {
         port.piecePlayed.PiecePlayedUp();
-        port.thisBoardPiece.soPieces.TempEffectValues[0] += context.NbrCaseLibre * amount[1];
-        port.DoDamageToEnnemi(amount[0] + port.thisBoardPiece.soPieces.TempEffectValues[0]);
+        port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[0] += amount[0];
+        port.DoDamageToEnnemi(port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[0]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.atk, null);
         context.NbrDeRepetition += 1;
     }
     public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
     {
         port.piecePlayed.RepeatedPieceUp();
-        port.thisBoardPiece.soPieces.TempEffectValues[0] += context.NbrCaseLibre * amount[1];
-        port.DoDamageToEnnemi(amount[0] + port.thisBoardPiece.soPieces.TempEffectValues[0]);
+        port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[0] += amount[0];
+        port.DoDamageToEnnemi(port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[0]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPieceRepeated(), PieceAnimations.TypeAnim.atk, declencheur);
         context.NbrDeRepetition += 1;
     }
---
+++ Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetShieldVampirism.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Audio.ProcessorInstance;
+
+[CreateAssetMenu(fileName = "ShieldVampirismEffet", menuName = "Effet/ShieldVampirism")]
+public class SoEffetShieldVampirism : SoEffet
+{
+    public override IEnumerator Effet(Context context, OutputPort port, List<int> amount, int tour)
+    {
+        port.piecePlayed.PiecePlayedUp();
+        BoardPiece piece = port.thisBoardPiece;
+        //yield return piece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.classic,null);
+
+        for (int i = 0; i < context.voisins.Count; i++)
+        {
+            BoardPiece voisin = context.voisins[i];
+            port.thisBoardPiece = voisin;
+            voisin.healthPoint -= piece.pieceInfo.soPiece.BaseEffectValues[0];
+            piece.pieceInfo.soPiece.TempEffectValues[0] += piece.pieceInfo.soPiece.BaseEffectValues[1];
+            voisin.shield += piece.pieceInfo.soPiece.TempEffectValues[0];
+            voisin.pieceAnimation.RefreshHealth(voisin);
+            yield return piece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.classic, null);
+            yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.shield, piece);
+        }
+        port.thisBoardPiece = piece;
+        context.NbrDeRepetition += 1;
+    }
+    public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
+    {
+        port.piecePlayed.RepeatedPieceUp();
+        BoardPiece piece = port.thisBoardPiece;
+
+        yield return piece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.classic, declencheur);
+
+        for (int i = 0; i < context.voisins.Count; i++)
+        {
+            BoardPiece voisin = context.voisins[i];
+            port.thisBoardPiece = voisin;
+            voisin.healthPoint -= piece.pieceInfo.soPiece.BaseEffectValues[0];
+            piece.pieceInfo.soPiece.TempEffectValues[0] += piece.pieceInfo.soPiece.BaseEffectValues[1];
+            voisin.shield += piece.pieceInfo.soPiece.TempEffectValues[0];
+            voisin.pieceAnimation.RefreshHealth(voisin);
+            if (i != 0) yield return piece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.classic, null);
+            yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.shield, piece);
+        }
+        port.thisBoardPiece = piece;
+        context.NbrDeRepetition += 1;
+
+    }
+
+}
