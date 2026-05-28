daa048c5d71d80385b34ea990920ccce1655959b
ravioli-sec <malo.tasse@gmail.com>
2026-05-28 18:04:10 +0200

    nerf + new piece

--- Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScaleIfNoShield.cs
+++ Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScaleIfNoShield.cs
@@ -3,21 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "DamageScaleEffet", menuName = "Effet/DamageScale")]
-public class SoEffetDamageScale : SoEffet
+[CreateAssetMenu(fileName = "DamageScaleIfNoShieldEffet", menuName = "Effet/DamageScaleIfNoShield")]
+public class SoEffetDamageScaleIfNoShield : SoEffet
 {
     public override IEnumerator Effet(Context context, OutputPort port, List<int> amount, int tour)
     {
+        if (port.thisBoardPiece.shield < 1)
+        {
+            port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1] += amount[0];
+        }
+
         port.piecePlayed.PiecePlayedUp();
-        port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1] += amount[0];
         port.DoDamageToEnnemi(port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.atk, null);
         context.NbrDeRepetition += 1;
     }
     public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
     {
+        if (port.thisBoardPiece.shield < 1)
+        {
+            port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1] += amount[0];
+        }
+
         port.piecePlayed.RepeatedPieceUp();
-        port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1] += amount[0];
         port.DoDamageToEnnemi(port.thisBoardPiece.pieceInfo.soPiece.TempEffectValues[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPieceRepeated(), PieceAnimations.TypeAnim.atk, declencheur);
         context.NbrDeRepetition += 1;
--- Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScaleWithEmpty.cs
+++ Assets/Scripts/SO_Scripts/Pieces&Effets_SO_Scripts/Effets/TousLesEffets/SoEffetDamageScaleWithEmpty.cs
@@ -10,6 +10,7 @@ public class SoEffetDamageScaleWithEmpty : SoEffet
     public override IEnumerator Effet(Context context, OutputPort port, List<int> amount, int tour)
     {
         port.piecePlayed.PiecePlayedUp();
+        port.thisBoardPiece.soPieces.TempEffectValues[1] -= 3;
         port.thisBoardPiece.soPieces.TempEffectValues[1] += context.NbrCaseLibre * amount[0];
         port.DoDamageToEnnemi(port.thisBoardPiece.soPieces.TempEffectValues[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPiecePlayed(), PieceAnimations.TypeAnim.atk, null);
@@ -18,6 +19,7 @@ public class SoEffetDamageScaleWithEmpty : SoEffet
     public override IEnumerator RepeatEffet(Context context, OutputPort port, List<int> amount, int tour, BoardPiece declencheur)
     {
         port.piecePlayed.RepeatedPieceUp();
+        port.thisBoardPiece.soPieces.TempEffectValues[1] -= 3;
         port.thisBoardPiece.soPieces.TempEffectValues[1] += context.NbrCaseLibre * amount[0];
         port.DoDamageToEnnemi(port.thisBoardPiece.soPieces.TempEffectValues[1]);
         yield return port.thisBoardPiece.pieceAnimation.PlayAnimations(port.piecePlayed.GetPieceRepeated(), PieceAnimations.TypeAnim.atk, declencheur);
