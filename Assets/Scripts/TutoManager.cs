6665b6b5fe05ad0e8f171615cf7db8582aee945b
Ferdinand MOURETTE <f_mourette@stu-bellecour.fr>
2026-05-28 15:39:17 +0200

    r

---
+++ Assets/Scripts/TutoManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TutoManager : MonoBehaviour
+{
+    
+
+    public enum Tutos
+    {
+        Tuto1,
+        Tuto2,
+        Tuto3,
+        Tuto4,
+        Tuto5,
+    }
+}
